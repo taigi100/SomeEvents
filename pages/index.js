c907a9484dfe1539d6e95e91967b191f98b0771c
taigi100 <contact@taigi100.com>
2023-02-11 20:50:29 +0200

    Update title

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Timisoara Events</title>
+        <title>Events</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
